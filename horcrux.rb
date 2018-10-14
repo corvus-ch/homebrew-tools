e330b27c0eb555be5844fd4dbb565a31cca58d42
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-14 16:20:50 +0200

    Brew formula update for horcrux version 0.0.1

--- horcrux.rb
+++ horcrux.rb
@@ -3,7 +3,7 @@ class Horcrux < Formula
   homepage "https://github.com/corvus-ch/horcrux"
   url "https://github.com/corvus-ch/horcrux/releases/download/0.0.1/horcrux_0.0.1_darwin_amd64.tar.gz"
   version "0.0.1"
-  sha256 "bdd26134d891ebf2dfcdad3b90a662e5b8fadfeff72dd8329e4bf163d437637e"
+  sha256 "38b4acd3694de8dd43e41312277f8298c6fb48d51a012d67cff48605d481c49b"
 
   def install
     bin.install "horcrux"
