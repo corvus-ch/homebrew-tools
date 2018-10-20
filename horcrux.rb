a1a5f98b0131f5090aa1de733c44aea640f139cf
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-20 10:36:00 +0200

    Brew formula update for horcrux version 1.1.0

--- horcrux.rb
+++ horcrux.rb
@@ -1,9 +1,9 @@
 class Horcrux < Formula
   desc "A helper for preparing backups of data worth protecting"
   homepage "https://github.com/corvus-ch/horcrux"
-  url "https://github.com/corvus-ch/horcrux/releases/download/1.0.0/horcrux_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "2d14cdd12a1b98e4e2911e7c47c01b86b4f975f41fd7f02547b931815fde97bc"
+  url "https://github.com/corvus-ch/horcrux/releases/download/1.1.0/horcrux_1.1.0_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "3f7dd05b0e7d3c3aca0d2630909c881475478d776147f170ede3b29602b98519"
 
   def install
     bin.install "horcrux"
