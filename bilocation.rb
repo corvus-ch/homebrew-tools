3f790fea44c03e9b627abb4fb78aa0f53df95bc2
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-28 22:24:22 +0100

    Brew formula update for bilocation version v0.2.1

--- bilocation.rb
+++ bilocation.rb
@@ -2,9 +2,9 @@
 class Bilocation < Formula
   desc "A tagging file system"
   homepage "https://github.com/corvus-ch/tagfuse"
-  url "https://github.com/corvus-ch/bilocation/releases/download/v0.2.1-alpha1/bilocation_0.2.1-alpha1_darwin_amd64.tar.gz"
-  version "0.2.1-alpha1"
-  sha256 "d8f58434d48596764a7fe96c473156519fdc9f5bc09a560cddda5ea414f79a93"
+  url "https://github.com/corvus-ch/bilocation/releases/download/v0.2.1/bilocation_0.2.1_darwin_amd64.tar.gz"
+  version "0.2.1"
+  sha256 "c868ccbb586d84dc70b04aff8cb4d94010ace19124888896ec6cbcac8820d0fe"
 
   def install
     bin.install "bilocation"
